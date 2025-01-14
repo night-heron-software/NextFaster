b342342239bb403397872dc0e739d99cb67be317
Jeff Romine <jeff@nightheron.dev>
2025-01-14 13:48:28 -0700

    Simplify drizzle config

--- drizzle.server.ts
+++ drizzle.server.ts
@@ -1,25 +1,3 @@
-// File: drizzle.server.ts
+import { db } from "./src/db";
 
-import { neonConfig, Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
-import { WebSocket } from "ws";
-import * as schema from "./src/db/schema";
-
-const connectionString =
-  process.env.NODE_ENV === "production"
-    ? process.env.POSTGRES_URL
-    : process.env.LOCAL_POSTGRES_URL;
-
-if (process.env.NODE_ENV === "production") {
-  neonConfig.webSocketConstructor = WebSocket;
-  neonConfig.poolQueryViaFetch = true;
-} else {
-  neonConfig.wsProxy = (host) => `${host}:54330/v1`;
-  neonConfig.useSecureWebSocket = false;
-  neonConfig.pipelineTLS = false;
-  neonConfig.pipelineConnect = false;
-}
-
-const pool = new Pool({ connectionString });
-
-export const db = drizzle(pool, { schema });
+export { db };
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,25 @@
-import { db } from "../../drizzle.server";
+// File: drizzle.server.ts
 
-export { db };
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { WebSocket } from "ws";
+import * as schema from "./schema";
+
+const connectionString =
+  process.env.NODE_ENV === "production"
+    ? process.env.POSTGRES_URL
+    : process.env.LOCAL_POSTGRES_URL;
+
+if (process.env.NODE_ENV === "production") {
+  neonConfig.webSocketConstructor = WebSocket;
+  neonConfig.poolQueryViaFetch = true;
+} else {
+  neonConfig.wsProxy = (host) => `${host}:54330/v1`;
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.pipelineTLS = false;
+  neonConfig.pipelineConnect = false;
+}
+
+const pool = new Pool({ connectionString });
+
+export const db = drizzle(pool, { schema });
